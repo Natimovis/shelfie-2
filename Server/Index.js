a0458a9496257a3f9c13671530b683620361e908
Nathan Visser <Nathan@Macs-MacBook-Pro-63.local>
2019-10-21 14:53:10 -0500

    commit

---
+++ Server/Index.js
@@ -0,0 +1,23 @@
+require ("dotenv").config();
+const express = require("express");
+const massive = require("massive");
+const controller = require("./controller");
+
+const app = express();
+
+const { SERVER_PORT, CONNECTION_STRING } = process.env;
+
+massive(CONNECTION_STRING)
+  .then(dbInstance => {
+    app.set("db", dbInstance);
+  })
+  .catch(err => console.log(err));
+
+  app.use(express.json());
+
+  
+
+const port = SERVER_PORT || 6505;
+app.listen(port, () => {
+    console.log(`The server is listening on the super metal port ${port}.`);
+});
---
+++ db/create_product.sql
@@ -0,0 +1,1 @@
+INSERT INTO test ( test_name, test_price, test_image_url ) VALUES ('Nathan ', 29, 'test image url 2');
